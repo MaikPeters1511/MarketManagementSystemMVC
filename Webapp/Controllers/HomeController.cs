f27f9c1c15927c2ed51aa1e58771b6cec706aebc
Maiki <bewerbung@maikpeters.org>
2024-10-16 10:06:42 +0200

    Add MVC support with HomeController
    Introduced MVC pattern by adding necessary services in Program.cs and creating a HomeController with a basic Index action. Enabled routing, HTTPS redirection, and authorization middleware to facilitate MVC functionality.

---
+++ Webapp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webapp.Controllers;
+
+public class HomeController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- Webapp/Program.cs
+++ Webapp/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
